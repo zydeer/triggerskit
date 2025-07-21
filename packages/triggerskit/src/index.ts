dd281099f8d97ed0b21a55d4f6be86d557327a86
arshad <arshadpyaseen@gmail.com>
2025-07-21 18:27:45 +0530

    chore: update

--- packages/triggerskit/src/index.ts
+++ packages/triggerskit/src/index.ts
@@ -1,3 +1,7 @@
 export function greet(name: string): string {
 	return `Hello, ${name}!`
 }
+
+export function sum(num1: number, num2: number): number {
+	return num1 + num2
+}
--- tests/dev.ts
+++ tests/dev.ts
@@ -1,11 +1,16 @@
-import { greet } from 'triggerskit'
+import { greet, sum } from 'triggerskit'
 
 Bun.serve({
 	port: 4000,
 	routes: {
 		'/': {
 			GET: () => {
-				return new Response(greet('John'))
+				return new Response(greet('Arshad'))
+			},
+		},
+		'/sum': {
+			GET: () => {
+				return new Response(sum(5, 5).toString())
 			},
 		},
 	},
