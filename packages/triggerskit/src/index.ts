6a14e4bccc5f6dbb84e21efc94bdee9ad4c34d89
arshad <arshadpyaseen@gmail.com>
2025-07-21 16:02:53 +0530

    chore: update

--- packages/triggerskit/src/index.ts
+++ packages/triggerskit/src/index.ts
@@ -1,3 +1,3 @@
 export function greet(name: string): string {
-	return `Hello, ${name}! Welcome to the triggerskit packages`
+	return `Hello, ${name}!`
 }
--- packages/triggerskit/test/index.test.ts
+++ packages/triggerskit/test/index.test.ts
@@ -2,7 +2,5 @@ import { expect, test } from 'bun:test'
 import { greet } from '../src'
 
 test('triggerskit package should greet correctly', () => {
-	expect(greet('World')).toBe(
-		'Hello, World! Welcome to the triggerskit package.',
-	)
+	expect(greet('World')).toBe('Hello, World!')
 })
